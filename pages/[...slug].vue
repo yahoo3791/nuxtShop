4425f5a38d8a3b92813513aa072dfc674866e67a
jie <yahoo3791@gmail.com>
2024-02-04 17:16:25 +0800

    feat: 導向錯誤頁面

---
+++ pages/[...slug].vue
@@ -0,0 +1,15 @@
+<script setup>
+const router = useRouter().back();
+</script>
+
+<template>
+  <div class="container text-center vh-100 mx-auto">
+    <div class="position-relative top-50 translate-middle-y lh-lg">
+      <div class="spinner-border" role="status">
+        <span class="visually-hidden">Loading...</span>
+      </div>
+      <h2 class="fs-1">Page Not Found</h2>
+      <p class="fs-3">馬上導向正確頁面</p>
+    </div>
+  </div>
+</template>
