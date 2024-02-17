c57f199a8536f27dd96acce5b6a331b9f56384b7
jie <yahoo3791@gmail.com>
2024-02-17 22:14:17 +0800

    refactor: 重構商品頁

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -1,21 +1,48 @@
 <script setup lang="ts">
 const route = useRoute();
-console.log(route.params.id);
 
 // let products: globalThis.Ref<{}>
 
-// const config = useRuntimeConfig();
-// const public_api = config.public.api;
-// const public_path = config.public.path;
-const { data } = useFetch("/api/products");
+const config = useRuntimeConfig();
+const public_api = config.public.api;
+const public_path = config.public.path;
+// ${process.env.VUE_APP_API}api/${process.env.VUE_APP_PATH}/products/all
+
+const getProducts = ref();
+const data = useFetch(`${public_api}api/${public_path}/products/all`).then(
+  (res) => {
+    getProducts.value = res.data.value.products;
+  }
+);
 // const getData = async () => {
 // const api = `${public_api}api/${public_path}/products/all`;
 // const data = await useFetch(api).then(res => {
-//   changeProducts.value = res.data.value.products;
 //   console.log(res);
 // })
 // let urlId = route.params.id;
 // };
+const breadcrumbName = ref();
+const getbreadcrumb = () => {
+  switch (route.params.id) {
+    case "cake":
+      breadcrumbName.value = "蛋糕";
+      break;
+    case "chocolate":
+      breadcrumbName.value = "巧克力";
+      break;
+    case "brownie":
+      breadcrumbName.value = "布朗尼";
+      break;
+    case "cupcake":
+      breadcrumbName.value = "杯子蛋糕";
+      break;
+    case "all":
+      breadcrumbName.value = "全部商品";
+      break;
+    default:
+      return;
+  }
+};
 const renderCarts = () => {};
 const addCart = (item, e) => {};
 const addFav = (item) => {};
@@ -28,6 +55,7 @@ const searchProducts = () => {};
 onMounted(() => {
   // getData();
   // updateFav();
+  getbreadcrumb();
 });
 
 // const updateProducts = computed(() => {
@@ -61,93 +89,95 @@ onMounted(() => {
     id="content"
     >:::</a
   >
-  <div class="container-fluid pt-utility auto-height">
-    <div class="row auto-height-content">
-      <div class="col-12 col-md-9 mx-auto p-0">
-        <div class="position-relative">
-          <img
-            src="@/assets/images/banner/productBanner.png"
-            class="w-100 productBanner"
-            alt="全部商品圖片"
-          />
-        </div>
-      </div>
-      <div
-        class="col-12 col-md-9 mx-auto p-0 d-flex align-items-center justify-content-between"
-      >
-        <div>
-          <i
-            class="bi bi-search d-inline-block px-2 fs-4 cursor-pointer"
-            @click="search = !search"
-            @keypress="search = !search"
-          />
-          <label for="search">
-            <input
-              type="text"
-              name="search"
-              v-model="message"
-              :class="{ 'd-none': search }"
-              class="carts-input w-100 border-0 border-bottom text-white"
-            />
-          </label>
-        </div>
-        <div
-          class="ps-3 px-md-0 d-flex flex-wrap justify-content-end align-items-center my-3"
+  <div class="container">
+    <div class="row">
+      <div class="col-12">
+        <a
+          class="accesskey position-absolute no-print start-0"
+          href="#"
+          title="主要內容區"
+          accesskey="C"
+          id="content"
+          >:::</a
         >
-          <label
-            for="sort"
-            class="text-white tracking-wide font-medium pe-2 pe-md-0 pb-md-1 d-inline-block"
-            >顯示方法
-            <div
-              class="bg-white d-inline-block"
-              style="max-width: 300px; border-radius: 5px"
-            >
-              <select
-                name="sort"
-                id="sort"
-                class="text-black tracking-wide font-medium px-4 py-1 border-0"
-                @change="onChange($event)"
+        <!-- 麵包屑 -->
+        <el-breadcrumb separator="/">
+          <el-breadcrumb-item
+            ><a href="/" title="首頁">首頁</a></el-breadcrumb-item
+          >
+          <el-breadcrumb-item>商品</el-breadcrumb-item>
+          <el-breadcrumb-item>{{ breadcrumbName }}</el-breadcrumb-item>
+        </el-breadcrumb>
+      </div>
+      <div class="col-12">
+        <img
+          src="@/assets/images/banner/productBanner.png"
+          class="w-100 productBanner"
+          alt=""
+        />
+        <div class="d-flex align-items-center justify-content-between">
+          <div>
+            <i class="bi bi-search d-inline-block pe-2 fs-4" />
+            <label for="search">
+              <input
+                type="text"
+                name="search"
+                class="w-100 border-0 bg-dark text-white border-bottom"
+                placeholder="查詢商品"
+                title="查詢商品"
+              />
+            </label>
+          </div>
+          <div
+            class="ps-3 px-md-0 d-flex flex-wrap justify-content-end align-items-center my-3"
+          >
+            <label
+              for="sort"
+              class="pe-2 pe-md-0 pb-md-1"
+              >顯示方法
+              <div
+                class="d-inline-block"
+                style="max-width: 300px;"
               >
-                <option
-                  selected="selected"
-                  disabled="disabled"
-                  class="d-n"
-                  value=""
+                <select
+                  name="sort"
+                  id="sort"
+                  class="bg-dark text-white px-4 py-1 border-0 border-bottom"
+                  title="選擇顯示方法"
+                  @change="onChange($event)"
                 >
-                  選擇顯示方法
-                </option>
-                <option value="熱銷商品">熱銷商品</option>
-                <option value="價格排序低到高">價格排序低到高</option>
-                <option value="價格排序高到低">價格排序高到低</option>
-              </select>
-            </div>
-          </label>
+                  <option
+                    disabled="disabled"
+                    value=""
+                  >
+                    選擇顯示方法
+                  </option>
+                  <option value="熱銷商品" title="熱銷商品">熱銷商品</option>
+                  <option value="價格排序低到高" title="價格排序低到高">價格排序低到高</option>
+                  <option value="價格排序高到低" title="價格排序高到低">價格排序高到低</option>
+                </select>
+              </div>
+            </label>
+          </div>
         </div>
       </div>
-      <div class="col-12 col-md-9 mx-auto">
-        <div class="row">
-          <div
-            class="col-6 col-md-6 col-lg-4 col-xl-3 mb-5"
-            v-for="item in data"
-            :key="item.id"
-          >
-            <div
-              class="card text-black"
-              style="width: 18rem"
-              @click="more(item.id)"
-              @keydown="more(item.id)"
-            >
-              <img
-                :src="item.imageUrl"
-                class="card-img-top"
-                :alt="item.imageUrl"
-              />
-              <div class="card-body">
-                <h5 class="card-title">{{ item.title }}</h5>
-                <p class="card-text">{{ item.content }}</p>
-                <a href="#" class="btn btn-primary">加入購物車</a>
-              </div>
-            </div>
+    </div>
+    <div class="row justify-content-between">
+      <div
+        class="col-sm-12 col-md-6 col-lg-4 col-xl-3 mb-5"
+        v-for="item in getProducts"
+        :key="item.id"
+      >
+        <div
+          class="card text-black"
+          @click="more(item.id)"
+          @keydown="more(item.id)"
+        >
+          <img :src="item.imageUrl" class="card-img-top" :alt="item.imageUrl" />
+          <div class="card-body">
+            <h5 class="card-title">{{ item.title }}</h5>
+            <p class="card-text">{{ item.content }}</p>
+            <a href="#" class="btn btn-primary" title="加入購物車">加入購物車</a>
           </div>
         </div>
       </div>
